a3b7f1f38ef4b3d1681068ebd673d797b661cb2c
Mayra234 <alejandradiavanera@gmail.com>
2022-03-24 22:22:27 -0500

    Reto 9 finalizado

---
+++ reto7/script.js
@@ -0,0 +1,8 @@
+function removeVowels(a) {
+  a.split("a", "e", "i", "o", "u");
+  const e = a.join("");
+}
+
+console.log(removeVowels("Mayra Alejandra"));
+
+// terminar
---
+++ reto9/script.js
@@ -0,0 +1,8 @@
+function determineNumber(number) {
+  if (number % 2 == 0) {
+    return "Es par";
+  } else {
+    return "Es impar";
+  }
+}
+console.log(determineNumber(1));
