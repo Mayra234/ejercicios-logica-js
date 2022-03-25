075e5d5e5ebedf51f6a039ff127d6b46143a35f7
Mayra234 <alejandradiavanera@gmail.com>
2022-03-24 22:46:03 -0500

    Corrección del reto 10

--- reto10/script.js
+++ reto10/script.js
@@ -1,8 +1,8 @@
-function calculateHypotenuse(lade1, lade2, h) {
-  if (lade1 <= 0 || lade2 <= 0) {
-    return "El lado del triángulo no puede ser menor o igual a 0";
+function calculateHypotenuse(side1, side2) {
+  if (side1 <= 0 || side2 <= 0) {
+    throw "El lado del triángulo no puede ser menor o igual a 0";
   }
-  return (h = Math.pow(lade1, 2) + Math.pow(lade2, 2));
+  return Math.pow(side1, 2) + Math.pow(side2, 2);
 }
 
 console.log(calculateHypotenuse(5, 1));
